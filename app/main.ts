c493b12164df0b48c55804d6c37cc518e949e405
Jay L. Ginete <jginete@innovuze.com>
2016-10-06 16:34:14 +0800

    Added font-awesome Still a mess!

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap'
+
 require('zone.js/dist/zone');
+require('font-awesome/css/font-awesome.css');
+require('bootstrap/dist/css/bootstrap.css')
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-
-import $ = require('jquery');
-import bootstrap = require('bootstrap');
 
 const platform = platformBrowserDynamic();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
 
@@ -34,13 +35,8 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.js$/i,
-                loader: 'script-loader',
-                exclude: /node_modules/
-            },
-            {
                 test: /\.css$/i,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                loader: 'style!css?sourceMap'
             },
             {
                 test: /\.(jpe?g|png|gif|svg([\?]?.*))$/i,
@@ -50,17 +46,37 @@ module.exports = {
                     'image?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
             },
-            { 
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-                loader: 'url-loader?limit=100000' 
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, 
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, 
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            }, 
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            }, 
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin('[name].[chunkhash].css'),
+        new ExtractTextPlugin('[name].[chunkhash].js'),
         new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
             rootAssetPath: rootAssetPath,
             ignorePaths: ['/styles', '/scripts']
-        })
+        }),
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+       })
     ]
 };
